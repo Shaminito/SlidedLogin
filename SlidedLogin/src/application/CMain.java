5e3bd48c603cc0ea60fc62b9bace8e553c39783a
Victor <Victor@DESKTOP-V3FBT3M>
2018-12-01 13:54:01 +0100

    Proyecto Finalizado

--- SlidedLogin/src/application/CMain.java
+++ SlidedLogin/src/application/CMain.java
@@ -1,5 +1,84 @@
 package application;
 
-public class CMain {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+public class CMain implements Initializable{
+
+	@FXML
+	AnchorPane pane2;
+	
+	@FXML
+	AnchorPane pane3;
+	
+	@FXML
+	AnchorPane pane4;
+	
+	FadeTransition ftPane2;
+	FadeTransition ftPane3;
+	FadeTransition ftPane4;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		backgroundAnimation();
+	}
+
+	private void backgroundAnimation() {
+		ftPane4 = new FadeTransition(Duration.seconds(4), pane4);
+		ftPane3 = new FadeTransition(Duration.seconds(4), pane3);
+		ftPane2 = new FadeTransition(Duration.seconds(4), pane2);
+
+		ftPane4.setFromValue(1);
+		ftPane4.setToValue(0);
+		ftPane4.play();
+
+		ftPane4.setOnFinished(event -> {
+	
+			ftPane3.setFromValue(1);
+			ftPane3.setToValue(0);
+			ftPane3.play();
+					
+			ftPane3.setOnFinished(event1 -> {
+				ftPane2.setFromValue(1);
+				ftPane2.setToValue(0);
+				ftPane2.play();
+
+				ftPane2.setOnFinished(event2 -> {
+
+					ftPane2.setFromValue(0);
+					ftPane2.setToValue(1);
+					ftPane2.play();
+
+					ftPane2.setOnFinished(event3 -> {
+
+						ftPane3.setFromValue(0);
+						ftPane3.setToValue(1);
+						ftPane3.play();
+
+						ftPane3.setOnFinished(event4 -> {
+
+							ftPane4.setFromValue(0);
+							ftPane4.setToValue(1);
+							ftPane4.play();
+
+							ftPane4.setOnFinished(event5 -> {
+
+								backgroundAnimation();
+							});
+						});
+
+					});
+
+				});
+				
+			});
+		});
+	}
 }
--- SlidedLogin/src/application/Main.java
+++ SlidedLogin/src/application/Main.java
@@ -1,13 +1,26 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("SlidedLogin.fxml"));
+		AnchorPane pane = loader.load();
 		
+		Scene scene = new Scene(pane);
+		
+		primaryStage.setScene(scene);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
